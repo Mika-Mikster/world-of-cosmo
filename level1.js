f0900250fea8bc3e724bd4ab86252cab259f16b3
Michaëlya de Bruin <micky_de_bruin@hotmail.com>
2016-08-09 10:05:03 +0200

    First part done

---
+++ level1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Michaëlya on 2016-08-09.
+ */
+function enterCosmo() {
+    alert('The page starts rippling and fluorescent colors start spilling out as you are pulled into the flyer. You have entered the World of Cosmo...');
+    
+}
+
+var userChoice1 = "something";
+if (userChoice1){
+
+} else {
+
+};
