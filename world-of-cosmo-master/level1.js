94b1243e78821699098bacd899d86f5a93fb7180
samuel <reikoryuu@gmail.com>
2016-08-10 11:04:48 +0200

    Update Angular Routing
    Added Angular Routing. Including fixed typed for Dialog One and Two. Button console error "style" Dialog Two, button not included.

---
+++ world-of-cosmo-master/level1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Michaëlya on 2016-08-09.
+ */
+function enterCosmo() {
+    swal( {
+        title: "",
+        text: 'The page starts rippling and fluorescent colors start spilling out as you are pulled into the flyer. You have entered the World of Cosmo...',
+        type: "",
+        confirmButtonText: "Continue"
+    });
+}
