e3abf4a47953286b869a05e0de60c949c6eec4a7
Michaëlya de Bruin <Mika-Mikster@users.noreply.github.com>
2016-08-12 12:34:58 +0200

    Delete angular.js

--- js/angular.js
+++
@@ -1,8 +0,0 @@
-angular.module('windowExample', [])
-    .controller('ExampleController', ['$scope', '$window', function($scope, $window) {
-        $scope.greeting = 'Hello, World!';
-        $scope.doGreeting = function(greeting) {
-            $window.alert(greeting);
-        };
-    }]);
-
