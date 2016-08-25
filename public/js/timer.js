def543bdd8eafc7726d2fafb54cb82a2cd12a086
samuel <reikoryuu@gmail.com>
2016-08-25 23:26:42 +0200

    no spaghetti timer
    remove all spaghetti code

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,76 +1,27 @@
-var dialog1 = document.getElementById('dialog1');
-var dialog2 = document.getElementById('dialog2');
-var dialog3 = document.getElementById('dialog3');
-var dialog4 = document.getElementById('dialog4');
-var timer = document.querySelectorAll(".timer");
-var time;
+var dialogs = ['dialog1', 'dialog2', 'dialog3', 'dialog4'];
 
-dialog1.classList.add("fadeIn");
 
-function timed () {setTimeout(function () {
-    for (var i = 0; i < timer.length; i++) {
-        timer[i].style.visibility = 'visible';
-    }
-}, time);
-}
-
-function dialogSwitch() {
-    if (dialog2 != null) {
-        console.log("Dialog 2 is present.");
-        setTimeout(function () {
-            dialog2.classList.add("fadeIn");
-            dialog2.style.visibility = 'visible';
-            dialog1.classList.add("fade");
-        }, 10000);
-        // second if
-        if (dialog3 != null) {
-            console.log("Dialog 3 is present.");
-            setTimeout(function () {
-                dialog3.classList.add("fadeIn");
-                dialog3.style.visibility = 'visible';
-                dialog2.classList.add("fade");
-            }, 30000);
-            // third if
-            if (dialog4 != null) {
-                console.log("Dialog 4 is present.");
-                setTimeout(function () {
-                    dialog4.classList.add("fadeIn");
-                    dialog4.style.visibility = 'visible';
-                    dialog3.classList.add("fade");
-                }, 40000);
-                //fourth if
-                if (dialog5 != null) {
-                    console.log("Dialog 5 is present.");
-                    setTimeout(function () {
-                        dialog5.classList.add("fadeIn");
-                        dialog5.style.visibility = 'visible';
-                        dialog4.classList.add("fade");
-                    }, 50000);
-                    // fourth else
-                } else {
-                    console.log("No dialog 4, loading buttons..");
-                    time = 40000;
-                    timed();
-                }
-            // third else
-            } else {
-                console.log("No dialog 4, loading buttons..");
-                time = 30000;
-                timed();
-            }
-            // second else
-        } else {
-            console.log("No dialog 3, loading buttons..");
-            time = 20000;
-            timed();
-        }
-        // first else
-    } else {
-        console.log("No dialog 2, loading buttons..");
-        time = 10000;
-        timed();
+function listen() {
+    for (var i = 0; i < array.length; i++){
+        dialogs[i].addEventListener('mouseover', lightUp, false);
+        dialogs[i].addEventListener('mouseout', lightOff, false);
     }
 
+// MOUSEOVER DIALOG \\
+function lightUp(){
+    console.log("LightUp!");
+    // IF "name".classList.fade === true
+    // "name".classlist.remove('fade')
+    // "name".classlist.add('check')
+    // ELSE console.log("Too soon.");
+    dialog1.classList.remove('fade');
 }
 
-window.onload = dialogSwitch();
+function lightOff(){
+    // IF "name".classList.check === true
+    // "name".classlist.remove('check')
+    // "name".classlist.add('fade')
+    // ELSE console.log("Too soon.");
+    console.log("LightOff!");
+    dialog1.classList.add('fade');
+}
--- public/js/view-app.js
+++ public/js/view-app.js
@@ -41,6 +41,10 @@ viewApp.config(['$routeProvider',
             templateUrl: '/dialog/dialogFiveB.html',
             controller: 'dialogFiveBController'
         }).
+        when('/dialogSix', {
+            templateUrl: '/dialog/dialogSix.html',
+            controller: 'dialogSixBController'
+        }).
         otherwise({
             redirectTo: '/dialogOne'
         });
@@ -67,16 +71,17 @@ viewApp.controller('dialogTwoController', function($scope, $http, testService) {
             angular.toJson($scope.itemsbag1)).success(
             function() {
                 testService.setTrigger("Item from dialog 2");
+                $scope.load();
             });
     };
-
 });
 
-viewApp.controller('dialogThreeController', function($scope, $http, testService) {
-    testService.setTrigger("Item from dialog 3");
+viewApp.controller('dialogThreeController', function($scope, testService, $http) {
+
+    testService.setTrigger("Item from dialog 2");
 
     $scope.itemsbag2 = {
-        id: 0,
+        id: 2,
         itemName: "Bottle of Water",
         itemDescr: "An ingredient used for ......",
         itemAbi: "It can be mixed with other ingredients",
@@ -90,6 +95,7 @@ viewApp.controller('dialogThreeController', function($scope, $http, testService)
         ).success(
             function() {
                 testService.setTrigger("Item from dialog 3");
+                $scope.load();
             });
     };
 });
@@ -98,6 +104,48 @@ viewApp.controller('dialogFourAController', function($scope, $http, testService,
     $timeout(function(){
         testService.setTrigger("Delay save item from dialog 3");
     }, 2000);
+
+    $scope.itemsbag5 = {
+        id: 0,
+        itemName: "Bottle of Water",
+        itemDescr: "An ingredient used for ......",
+        itemAbi: "It can be mixed with other ingredients",
+        itemHit: "15",
+        itemHeal: "15"
+    };
+
+    $scope.delete = function (Name) {
+        $http.delete("/itemsbag/" + "Bottle of Water").success(function() {
+            testService.setTrigger("Deleted item from dialog 5A");
+            $scope.load();
+        });
+    };
+});
+
+viewApp.controller('dialogFourBController', function($scope, $http, testService) {
+    testService.setTrigger("Item from dialog 2");
+
+    $scope.itemsbag3 = {
+        id: 0,
+        itemName: "Bottle of Water",
+        itemDescr: "An ingredient used for ......",
+        itemAbi: "It can be mixed with other ingredients",
+        itemHit: "15",
+        itemHeal: "15"
+    };
+
+    $scope.save = function() {
+        $http.post(
+            '/itemsbag', angular.toJson($scope.itemsbag3)
+        ).success(
+            function () {
+                testService.setTrigger("Item from dialog 3");
+                $scope.load();
+            });
+    };
+});
+
+viewApp.controller('dialogFourCController', function($scope, $http, testService) {
 
     $scope.itemsbag4 = {
         id: 0,
@@ -108,51 +156,13 @@ viewApp.controller('dialogFourAController', function($scope, $http, testService,
         itemHeal: "15"
     };
 
-    $scope.delete = function (ID) {
-        $http.delete("/itemsbag/" + 2).success(function() {
-            testService.setTrigger("Deleted item from dialog 5A");
-        });
-    };
-});
-
-viewApp.controller('dialogFourBController', function($scope, $http, testService) {
-
-    $scope.itemsbag2 = {
-        id: 0,
-        itemName: "Bottle of Water",
-        itemDescr: "An ingredient used for ......",
-        itemAbi: "It can be mixed with other ingredients",
-        itemHit: "15",
-        itemHeal: "15"
-    };
-
     $scope.save = function() {
         $http.post(
-            '/itemsbag', angular.toJson($scope.itemsbag2)
+            '/itemsbag', angular.toJson($scope.itemsbag4)
         ).success(
             function () {
                 testService.setTrigger("Item from dialog 3");
-            });
-    };
-});
-
-viewApp.controller('dialogFourCController', function($scope, $http, testService) {
-
-    $scope.itemsbag2 = {
-        id: 0,
-        itemName: "Bottle of Water",
-        itemDescr: "An ingredient used for ......",
-        itemAbi: "It can be mixed with other ingredients",
-        itemHit: "15",
-        itemHeal: "15"
-    };
-
-    $scope.save = function() {
-        $http.post(
-            '/itemsbag', angular.toJson($scope.itemsbag2)
-        ).success(
-            function () {
-                testService.setTrigger("Item from dialog 3");
+                $scope.load();
             });
     };
 });
@@ -163,7 +173,7 @@ viewApp.controller('dialogFiveAController', function($scope, $http, $timeout, te
         testService.setTrigger("Delay delete item from dialog 4A");
     }, 3000);
 
-    $scope.itemsbag3 = {
+    $scope.itemsbag7 = {
         id: 0,
         itemName: "Bottle of Olive Oil",
         itemDescr: "An ingredient used for ......",
@@ -172,33 +182,45 @@ viewApp.controller('dialogFiveAController', function($scope, $http, $timeout, te
         itemHeal: "15"
     };
 
-    $scope.itemsbag4 = {
+    $scope.itemsbag8 = {
         id: 0,
         itemName: "Bottle of Water",
         itemDescr: "An ingredient used for ......",
         itemAbi: "It can be mixed with other ingredients",
         itemHit: "15",
         itemHeal: "15"
+    };
+
+    $scope.recipeShampoo = {
+        id: 0,
+        mixItemName: "Shampoo",
+        itemName1: "Bottle of Water",
+        itemName2: "Jar of Bicarbonate"
     };
 
 
     $scope.save = function () {
         $http.post(
-            '/itemsbag', angular.toJson($scope.itemsbag3)
+            '/itemsbag', angular.toJson($scope.itemsbag7)
         ).success(function () {
             $scope.load();
         });
         $http.post(
-            '/itemsbag', angular.toJson($scope.itemsbag4)
+            '/itemsbag', angular.toJson($scope.itemsbag8)
+        ).success(function () {
+            $scope.load();
+        });
+        $http.post(
+            '/recipe', angular.toJson($scope.recipeShampoo)
         ).success(function () {
             $scope.load();
         });
     };
-
+    testService.setTrigger("Item from dialog 2");
 });
 viewApp.controller('dialogFiveBController', function($scope, $http, testService) {
 
-    $scope.itemsbag3 = {
+    $scope.itemsbag6 = {
         id: 0,
         itemName: "Bottle of Water",
         itemDescr: "An ingredient used for ......",
@@ -207,12 +229,18 @@ viewApp.controller('dialogFiveBController', function($scope, $http, testService)
         itemHeal: "15"
     };
 
-    $scope.delete = function (ID) {
-        $http.delete("/itemsbag/" + 2).success(function() {
+    $scope.delete = function (Name) {
+        $http.delete("/itemsbag/" + "Bottle of Water").success(function() {
             testService.setTrigger("Deleted item from dialog 5A");
+            $scope.load();
         });
     };
 });
+viewApp.controller('dialogSixController', function($scope, $http, testService) {
+    // var showRecipe = document.getElementsByClassName(".level2");
+    // showRecipe.style.visibility = "visible";
+});
+
 
 // service
 viewApp.service('testService', function(){
@@ -225,6 +253,8 @@ viewApp.service('testService', function(){
         notification = notification2;
     };
 });
+
+
 
 // Itembag controller
 viewApp.controller ("itembagCtrl", function ($scope, $http, testService){
@@ -252,5 +282,103 @@ viewApp.controller ("itembagCtrl", function ($scope, $http, testService){
         this.hoverEdit = false;
     };
 
+    $scope.itemShampoo = ["Bottle of Water", "Jar of Bicarbonate"];
+
+    $scope.selection = [];
+
+    $scope.toggle = function (idx) {
+        var pos =
+            $scope.selection.indexOf($scope.itemsbags[idx].itemName);
+        if (pos == -1) {
+            $scope.selection.push($scope.itemsbags[idx].itemName);
+        } else {
+            $scope.selection.splice(pos, 1);
+        }
+
+        $scope.save = function () {
+            $http.get('/mixeditems').success(function (data, status, headers, config) {
+                $scope.mixeditems = data;
+            }).error(function (data, status, headers, config) {
+                console.log(status);
+                console.log(data);
+            });
+            console.log("Saving items for mixing");
+            var mix = document.getElementById("mix");
+            // Dit is een voorlopige check of beide scopes gelijk zijn aan elkaar
+            if ($scope.selection.length == $scope.itemShampoo.length ) {
+                var showMix = document.getElementById('createdItems');
+                showMix.style.visibility = "visible";
+
+                $http.delete("/itemsbag/" + $scope.itemShampoo[0]).success(function() {
+                    testService.setTrigger("Deleted items for mixing");
+                });
+                $http.delete("/itemsbag/" + $scope.itemShampoo[1]).success(function() {
+                    testService.setTrigger("Deleted items for mixing");
+                });
+
+                $scope.load();
+            }
+        }
+    };
+
     $scope.load();
+});
+
+
+// Recipe controller
+viewApp.controller ("recipeCtrl", function ($scope, $http, testService){
+    $scope.load = function() {
+
+        console.log("loading recipe");
+        $http.get('/recipe').success(function (data, status, headers, config)
+        {
+            $scope.recipes = data;
+        }).error(function (data, status, headers, config) {
+            console.log(status);
+            console.log(data);
+        });
+
+        testService.addNotification(
+            $scope.load
+        );
+    };
+    $scope.load();
+
+    $scope.hovered = function(){
+        console.log("everyday i'm hovering");
+        this.hoverShow = true;
+    };
+
+    $scope.nothovered = function(){
+        this.hoverShow = false;
+    };
+});
+
+
+// mixed items controller
+viewApp.controller ("mixeditemsCtrl", function ($scope, $http, testService){
+    $scope.load = function() {
+
+        console.log("loading mixed item");
+        $http.get('/mixeditems').success(function (data, status, headers, config)
+        { console.log(data);
+            $scope.mixeditems = data;
+        }).error(function (data, status, headers, config) {
+            console.log(status);
+            console.log(data);
+        });
+
+        testService.addNotification(
+            $scope.load
+        );
+    };
+    $scope.load();
+
+    $scope.hovered = function () {
+        this.hoverShow = true;
+    };
+
+    $scope.hoveredOut = function () {
+        this.hoverShow = false;
+    };
 });
